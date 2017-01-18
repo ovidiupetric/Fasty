5ceab8ec6ed3aaf5afc90b78bd3a43b840c9bcec
Pop Alexandru Radu <minealex2244@gmail.com>
2017-01-18 11:36:15 +0200

    Update Hospital.java

--- Hospital.java
+++ Hospital.java
@@ -9,7 +9,7 @@ public class Hospital extends Actor
     public void addedToWorld(World w)
     {
         //preventing from spawning on top of another object except for another building
-        while (isTouching(Boy.class) || isTouching(Ambulance.class))
+        while(isTouching(Boy.class) || isTouching(Ambulance.class))
         {
             int x=Greenfoot.getRandomNumber(getWorld().getWidth()-10);
             int y=Greenfoot.getRandomNumber(getWorld().getHeight()-10);
